43307e62e8388cced0678ce40d64e4672fa15bbe
David Hamann <git@dha.email>
2020-12-06 00:04:40 +0100

    command execution

--- ptb/ptb.py
+++ ptb/ptb.py
@@ -1,7 +1,26 @@
 import sys
 import logging
+import subprocess
+from typing import List
 from pathlib import Path
 from .config import Config
+
+
+def exec(command: List) -> subprocess.CompletedProcess:
+    assert isinstance(command, type([]))
+    logging.info('Running %s' % ' '.join(command))
+
+    try:
+        return subprocess.run(command, encoding='ascii',
+                              capture_output=True, check=True)
+    except subprocess.CalledProcessError as error:
+        logging.error(
+            'Command "%s" failed with code %d.\n%s\n%s',
+            ' '.join(command), error.returncode, error.stdout, error.stderr)
+    except FileNotFoundError:
+        logging.error('%s cannot be found' % command[0])
+
+    return False
 
 
 class Ptb:
@@ -10,19 +29,22 @@ class Ptb:
 
     def set_up(self):
         logging.info('Starting to set up pentest box')
+
+        if not exec(['id']):
+            return False
+
         return True
 
 
 def main():
     '''CLI entry point'''
+    result: bool = True
 
     logging.basicConfig(
         format='[%(levelname)s] %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         handlers=[logging.StreamHandler(sys.stdout)],
         level=logging.DEBUG)
-
-    res = True
     config_file = Path(sys.argv[1]) if len(sys.argv) > 1 else None
 
     confirm = 'y'  # input('Configure this box? [Y/n]: ')
@@ -31,8 +53,8 @@ def main():
         config = Config(config_file)
         if config.load():
             ptb = Ptb(config)
-            ptb.set_up()
+            result = ptb.set_up()
     else:
         print('Cancelling...')
 
-    sys.exit(0) if res else sys.exit(1)
+    sys.exit(0) if result else sys.exit(1)
