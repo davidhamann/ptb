7c13014d0f9a0b399055b6005ce2d8c2590896b3
David Hamann <git@dha.email>
2020-12-25 19:13:53 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ptb',
-    version='0.0.1',
+    version='0.1.0',
     packages=['ptb'],
     entry_points={
         'console_scripts': ['ptb=ptb.ptb:main']
